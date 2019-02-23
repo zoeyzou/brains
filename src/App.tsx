f50b6456f88d1fca1ca666785b5a2a7eb4787055
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-02-23 19:02:24 +0100

    add basic state and data fetching to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import React, { Component } from "react";
-import "./App.css";
-import Loader from "./components/Loader";
+import React, { Component } from 'react';
+import { GlobalStyle } from './utils/global-style';
+import { loadPersons } from './utils/api';
+import { Person } from './models/person';
 
-class App extends Component {
+type Props = {};
+
+type State = {
+  isLoadingPersons: boolean;
+  allPersons: Person[] | null;
+  errorMsg: string | undefined;
+};
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isLoadingPersons: false,
+      allPersons: null,
+      errorMsg: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoadingPersons: true });
+    loadPersons()
+      .then(res => this.setState({ allPersons: res }))
+      .catch((err: string) => this.setState({ errorMsg: err }));
+    this.setState({ isLoadingPersons: false });
+  }
+
   render() {
     return (
-      <div className='App'>
-        <Loader />
-      </div>
+      <>
+        <GlobalStyle />
+      </>
     );
   }
 }
