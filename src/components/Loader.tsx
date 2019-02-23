f948af48baee9f305d11bdc6f1be05a592ce46e6
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-02-23 19:23:31 +0100

    add theme to loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,26 +3,58 @@ import styled, { keyframes } from '../utils/styled-components';
 
 interface LoaderProps {
   className?: string;
+  loaderTheme?: 'dark' | 'light';
 }
 
 const _Loader: React.FunctionComponent<LoaderProps> = (props: LoaderProps) => {
   return <div className={props.className} />;
 };
 
-const typing = keyframes`
+_Loader.defaultProps = {
+  loaderTheme: 'dark',
+};
+
+const darkThemeColor = [
+  'rgba(255, 255, 255, .2)',
+  'rgba(255, 255, 255, .4)',
+  'rgba(255, 255, 255, 1)',
+];
+const lightThemeColor = [
+  'rgba(0, 0, 0, .2)',
+  'rgba(0, 0, 0, .4)',
+  'rgba(0, 0, 0, 1)',
+];
+
+const typing = (theme: 'dark' | 'light' | undefined) => keyframes`
   0% {
-    background-color: rgba(255,255,255, 1);
-    box-shadow: 20px 0px 0px 0px rgba(255,255,255,0.2), 40px 0px 0px 0px rgba(255,255,255,0.2);
+    background-color: ${
+      theme === 'dark' ? darkThemeColor[2] : lightThemeColor[2]
+    };
+    box-shadow: 20px 0px 0px 0px ${
+      theme === 'dark' ? darkThemeColor[0] : lightThemeColor[0]
+    }, 40px 0px 0px 0px ${
+  theme === 'dark' ? darkThemeColor[0] : lightThemeColor[0]
+};
   }
    
   25% { 
-    background-color: rgba(255,255,255, 0.4);
-    box-shadow: 20px 0px 0px 0px rgba(255,255,255,2), 40px 0px 0px 0px rgba(255,255,255,0.2);
+    background-color: ${
+      theme === 'dark' ? darkThemeColor[1] : lightThemeColor[1]
+    };
+    box-shadow: 20px 0px 0px 0px ${
+      theme === 'dark' ? darkThemeColor[2] : lightThemeColor[2]
+    }, ${theme === 'dark' ? darkThemeColor[0] : lightThemeColor[0]};
   }
    
   75% { 
-    background-color: rgba(255,255,255, 0.4);
-    box-shadow: 20px 0px 0px 0px rgba(255,255,255,0.2), 40px 0px 0px 0px rgba(255,255,255,1);
+    background-color: ${
+      theme === 'dark' ? darkThemeColor[1] : lightThemeColor[1]
+    };
+    box-shadow: 20px 0px 0px 0px ${
+      theme === 'dark' ? darkThemeColor[0] : lightThemeColor[0]
+    }, 40px 0px 0px 0px ${
+  theme === 'dark' ? darkThemeColor[2] : lightThemeColor[2]
+};
   }
 `;
 
@@ -30,7 +62,8 @@ const Loader = styled(_Loader)`
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  animation: ${typing} 1s linear infinite alternate;
+  animation: ${({ loaderTheme }) => typing(loaderTheme)} 1s linear infinite
+    alternate;
 `;
 
 export default Loader;
