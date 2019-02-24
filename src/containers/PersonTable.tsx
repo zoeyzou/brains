4491052a8481683112109837687bacef199b17fe
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-02-24 16:38:42 +0100

    compose table elements to person table with api calls and data populated

---
+++ src/containers/PersonTable.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import TableHead from '../components/TableHead';
+import { Person } from '../models/person';
+import { LoadingState } from '../models/loading-state';
+import { loadPersons } from '../utils/api';
+import Loader from '../components/Loader';
+import TableRow from '../components/TableRow';
+import TableBody from '../components/TableBody';
+import { getValueGroupsFromArrayOfObject } from '../utils/helper';
+
+type PersonTableProps = {
+  className?: string;
+};
+
+type PersonTableState = {
+  loadingState: LoadingState;
+  persons: Person[] | null;
+  errMsg: string | null;
+};
+
+class _PersonTable extends React.Component<PersonTableProps, PersonTableState> {
+  constructor(props: PersonTableProps) {
+    super(props);
+
+    this.state = {
+      loadingState: LoadingState.default,
+      persons: null,
+      errMsg: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loadingState: LoadingState.pending });
+    loadPersons()
+      .then(res =>
+        this.setState({ persons: res, loadingState: LoadingState.success })
+      )
+      .catch((err: string) =>
+        this.setState({
+          errMsg: err,
+          loadingState: LoadingState.failure,
+          persons: [],
+        })
+      );
+  }
+
+  getIds = (): number[] | undefined => {
+    if (this.state.persons) {
+      return this.state.persons.map(person => person.Id);
+    }
+  };
+
+  getDataWithoutId = () => {
+    const data = getValueGroupsFromArrayOfObject(this.state.persons || []);
+    data.forEach(items => {
+      items.splice(0, 1);
+      items.push('icon');
+    });
+    return data;
+  };
+
+  public render() {
+    const { className } = this.props;
+    const { loadingState, persons, errMsg } = this.state;
+
+    return (
+      <div className={className}>
+        <div className='wrapper'>
+          <TableHead
+            items={['Name', 'Year Of Birth', 'Children', 'Profession', '']}
+          />
+          {loadingState === LoadingState.pending ? (
+            <div className='loader'>
+              <Loader />
+            </div>
+          ) : (
+            <TableBody data={this.getDataWithoutId()} ids={this.getIds()} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+const PersonTable = styled(_PersonTable)`
+  display: flex;
+  height: calc(467 / 667 * 100%);
+
+  .wrapper {
+    width: 100%;
+    height: 100%;
+
+    .loader {
+      width: 100%;
+      margin-top: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  @media (min-width: 568px) {
+    justify-content: center;
+    .wrapper {
+      width: calc(1200 / 1920 * 100%);
+      margin-top: calc(15px + 1vmin);
+    }
+  }
+`;
+
+export default PersonTable;
