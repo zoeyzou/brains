ae513a5af42dddd394a695497aa6a953c90d3629
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-02-24 16:39:49 +0100

    add global styles, header and person table to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './utils/global-style';
-import { loadPersons } from './utils/api';
-import { Person } from './models/person';
+import Header from './components/Header';
+import PersonTable from './containers/PersonTable';
 
 type Props = {};
 
-type State = {
-  isLoadingPersons: boolean;
-  allPersons: Person[] | null;
-  errorMsg: string | undefined;
-};
-
-class App extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      isLoadingPersons: false,
-      allPersons: null,
-      errorMsg: undefined,
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ isLoadingPersons: true });
-    loadPersons()
-      .then(res => this.setState({ allPersons: res }))
-      .catch((err: string) => this.setState({ errorMsg: err }));
-    this.setState({ isLoadingPersons: false });
-  }
-
+class App extends Component<Props, {}> {
   render() {
     return (
       <>
         <GlobalStyle />
+        <Header />
+        <PersonTable />
       </>
     );
   }
